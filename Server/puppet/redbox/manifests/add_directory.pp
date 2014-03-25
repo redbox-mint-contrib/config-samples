5f03e4ca3fce31bf33e976eb3da7b0457f051915
mattRedbox <matt@redboxresearchdata.com.au>
2014-03-25 12:13:08 +1000

    removed puppet as adding as standalone to private repo.

--- Server/puppet/redbox/manifests/add_directory.pp
+++
@@ -1,9 +0,0 @@
-define redbox::add_directory($end_path = $title, $owner) {
-    file { "/opt/${end_path}":
-      ensure  => directory,
-      recurse => true,
-      owner   => $owner,
-      require => Add_systemuser[$owner],
-    }                                    
-}
-
